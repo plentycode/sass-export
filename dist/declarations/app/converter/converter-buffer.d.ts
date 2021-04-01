cc25f4702c037d5ead8a89f064e2d12c7c570567
zhouhuafei <1123486116@qq.com>
2021-04-01 23:39:59 +0800

    feat: 🎸 del dist

--- dist/declarations/app/converter/converter-buffer.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" />
-import { Converter } from './converter';
-export declare class ConverterBuffer extends Converter {
-    private inputBuffers;
-    constructor(inputBuffers: Buffer[], options?: IOptions);
-    getData(): Promise<any>;
-    getContent(): string;
-}
