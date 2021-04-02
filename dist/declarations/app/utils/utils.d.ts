1ef9dce49124aff91a63881c48e011d2240f4db3
zhouhuafei <1123486116@qq.com>
2021-04-02 12:37:19 +0800

    feat: 🎸 测试postinstall

--- dist/declarations/app/utils/utils.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class Utils {
-    static getDeclarationByName(declarations: IDeclaration[], name: string): IDeclaration;
-    static wrapCss(cssDeclaration: IDeclaration): string;
-    static unWrapValue(wrappedContent: string): string;
-}
